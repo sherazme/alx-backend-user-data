0f3d2b3bf883290c3c7a1262265339b7e5415fc4
Shiraz <you@example.com>
2024-11-13 02:49:24 +0000

    auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
+"""Route module for the API.
 """
-Route module for the API
-"""
+import os
 from os import getenv
-from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
+
+from api.v1.views import app_views
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
 auth = None
 auth_type = getenv('AUTH_TYPE', 'auth')
 if auth_type == 'auth':
@@ -21,10 +22,28 @@ if auth_type == 'basic_auth':
     auth = BasicAuth()
 
 
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """ Unauthorized handler """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """ Forbidden handler """
+    return jsonify({"error": "Forbidden"}), 403
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """Not found handler.
+    """
+    return jsonify({"error": "Not found"}), 404
+
+
 @app.before_request
 def authenticate_user():
-    """Authenticates a user before processing a request.
-    """
+    """ Authenticat user before processing request """
     if auth:
         excluded_paths = [
             '/api/v1/status/',
@@ -38,27 +57,6 @@ def authenticate_user():
                 abort(401)
             if user is None:
                 abort(403)
-
-
-@app.errorhandler(401)
-def unauthorized(error) -> str:
-    """ Unauthorized error handler
-    """
-    return jsonify({"error": "Unauthorized"}), 401
-
-
-@app.errorhandler(403)
-def forbidden(error) -> str:
-    """ Forbidden error handler
-    """
-    return jsonify({"error": "Forbidden"}), 403
-
-
-@app.errorhandler(404)
-def not_found(error) -> str:
-    """ Not found handler
-    """
-    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
