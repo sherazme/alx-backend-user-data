9da7e02ee5dff9490813f64b744c37a68d95aaf9
Shiraz <you@example.com>
2024-11-15 02:58:39 +0000

    session

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -31,3 +31,10 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """ Get current user from the request """
         return None
+
+     def session_cookie(self, request=None) -> str:
+        """Gets the value of the cookie named SESSION_NAME.
+        """
+        if request is not None:
+            cookie_name = os.getenv('SESSION_NAME')
+            return request.cookies.get(cookie_name)
